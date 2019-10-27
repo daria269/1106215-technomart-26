7e35b8322824370e55b45cbbbe92375d3de09289
daria <dasha_kravchenko_00@mail.ru>
2019-10-28 02:34:37 +0300

    f

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var buy = document.querySelector('.buy');
+var buypopup = document.querySelector('.modal-cart');
+var close =document.querySelector('.close-button');
+
+buy.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  buypopup.classList.add('modal-cart-show');
+});
+
+close.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  buypopup.classList.remove('modal-cart-show');
+})
