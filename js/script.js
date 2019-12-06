4cbab0aff4435388700d68f369556178b65c2ecf
daria <dasha_kravchenko_00@mail.ru>
2019-12-07 00:03:28 +0300

    сложно

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-var buy = document.querySelector('.buy');
-var buypopup = document.querySelector('.modal-cart');
+/* var buypopup = document.querySelector('.modal-cart');
 var close =document.querySelector('.close-button');
 
 buy.addEventListener('click', function (evt) {
@@ -11,3 +10,22 @@ close.addEventListener('click', function (evt) {
   evt.preventDefault();
   buypopup.classList.remove('modal-cart-show');
 })
+*/
+let divMap = document.querySelector('.divMap');
+let mapMini = document.querySelector('.map-mini');
+divMap.onclick = function () {
+  mapMini.style.overflow ='visible' ;
+};
+ let closeMap = document.querySelector('.close-map'); /*  не работает кнопка закрыть */
+ closeMap.onclick = function () {
+   mapMini.style.overflow ='hidden' ;
+   divMap.style.overflow = 'hidden' ;
+   console.log('нажата');
+ };
+
+let modal = document.querySelector('.modal');
+let writeBtn  = document.querySelector('.long-button');
+writeBtn.onclick = function () {
+  modal.style.display = 'block';
+
+}
